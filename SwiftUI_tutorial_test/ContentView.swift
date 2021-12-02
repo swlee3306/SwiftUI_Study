3cbf21f1c670ff83124292045e1faa9038fd1ae1
swlee <swlee3306@gmail.com>
2021-12-02 13:02:40 +0900

    Initial Commit

---
+++ SwiftUI_tutorial_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_tutorial_test
+//
+//  Created by sanguk lee on 2021/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_tutorial_test/SwiftUI_tutorial_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_tutorial_testApp.swift
+//  SwiftUI_tutorial_test
+//
+//  Created by sanguk lee on 2021/12/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_tutorial_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
